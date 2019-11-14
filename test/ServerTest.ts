191d875648e25a86381a9addbfc736a023ff70bd
Dan Curran <codetown@gmail.com>
2019-11-14 14:49:55 -0500

    Change test file location.

--- test/ServerTest.ts
+++ test/ServerTest.ts
@@ -1,9 +1,8 @@
 import * as httpClient from 'httpie';
-import * as Colyseus from "colyseus.js";
 import { DB } from '../src/database';
 import assert from "assert";
 
-describe("Integration", () => {
+describe("Server", () => {
 
   const url = "http://localhost:2567";
   const ws = "ws://localhost:2567";
@@ -15,14 +14,11 @@ describe("Integration", () => {
 
   const client = new Colyseus.Client(`${ws}`);
 
-  var shipBuilderRoom;
-
   describe("signup, login and renew", () => {
     after(async () => {
       await DB.init().then(() => {
         DB.$accounts.deleteOne({username: dummy_username});
         DB.$ships.deleteMany({username: dummy_username});
-        console.log("\nDone cleaning database for integration test");
       })
     })
 
@@ -66,39 +62,5 @@ describe("Integration", () => {
 
       access_token = response.data.token;
     });
-
-    it("should respont to JOIN /ShipBuilderRoom to allow the user to create a ship", async() => {
-      shipBuilderRoom = await client.joinOrCreate('ShipBuilderRoom', {token: access_token});
-      let promise = await new Promise((resolve) => {
-        shipBuilderRoom.onMessage((message) => {
-          console.log(message);
-          if(message.action == 'ships') {
-            assert.ok(message.ships);
-            if(Object.keys(message.ships).length === 0){
-              console.log('Attempting to build a ship');
-              shipBuilderRoom.send({action: 'create', ship: {
-                name: 'Test Ship Name',
-                body_visual: 1,
-                wing_visual: 1,
-                weapon_visual: 1,
-                engine_visual: 1,
-                primary_attack: 1,
-                secondary_attack: 1,
-                weapon_type: 1,
-                shield_type: 1,
-                hull_type: 1,
-                engine_type: 1
-              }});
-            } else {
-              resolve(message);
-            }
-          }
-        });
-      });
-      console.log(promise);
-
-    });
-
-
   });
 })
