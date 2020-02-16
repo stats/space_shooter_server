e06aa602c9cb1a11176d0e2e1ae19afcaad29223
Dan <codetown@gmail.com>
2020-02-16 08:27:11 -0500

    Update ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,9 +2,6 @@ module.exports = {
   apps : [{
     name: 'API',
     script: 'src/index.ts',
-
-    // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
-    args: 'one two',
     instances: 1,
     autorestart: true,
     watch: false,
@@ -24,8 +21,7 @@ module.exports = {
       key : "~/ssh/digital_ocean_private.pem",
       ref  : 'origin/master',
       repo : 'https://github.com/stats/space_shooter_server.git',
-      path : '/var/www/cindertron7.com/production',
-      'post-deploy' : 'npm install'
+      path : '/var/www/cindertron7.com/production'
     }
   }
 };
