cc286048b5c08036007f07a2022d19198c3fec39
Dan <codetown@gmail.com>
2020-02-16 13:23:18 -0500

    Apply decelleration and make braking more responsive.

--- src/behaviours/player/InputBehaviour.ts
+++ src/behaviours/player/InputBehaviour.ts
@@ -7,6 +7,8 @@ export class InputBehaviour extends Behaviour {
   horizontal_vector:number = 0;
   vertical_vector:number = 0;
 
+  drag_factor:number = 2;
+
   constructor(target) {
     super('input', target);
   }
@@ -34,16 +36,50 @@ export class InputBehaviour extends Behaviour {
     this.target.vertical_accelleration = Math.min(Math.max(this.target.vertical_accelleration, -this.target.getSpeed()), this.target.getSpeed());
   }
 
+  private decellerateHorizontal(deltaTime:number) {
+    if(this.target.horizontal_accelleration == 0) return;
+
+    let sign = Math.sign(this.target.horizontal_accelleration);
+    if( sign < 0 ) {
+      this.target.horizontal_accelleration += Math.min(this.target.getAccelleration()/this.drag_factor * (deltaTime / 1000 ), 0);
+    } else {
+      this.target.horizontal_accelleration -= Math.max(this.target.getAccelleration()/this.drag_factor * (deltaTime / 1000), 0);
+    }
+  }
+
+  private decellerateVertical(deltaTime:number) {
+    if(this.target.vertical_accelleration == 0) return;
+
+    let sign = Math.sign(this.target.vertical_accelleration);
+    if( sign < 0 ) {
+      this.target.vertical_accelleration += Math.min(this.target.getAccelleration()/this.drag_factor * (deltaTime / 1000 ), 0);
+    } else {
+      this.target.vertical_accelleration -= Math.max(this.target.getAccelleration()/this.drag_factor * (deltaTime / 1000), 0);
+    }
+  }
+
   public onUpdate(deltaTime:number) {
     if(this.horizontal_vector != 0) {
       this.target.horizontal_accelleration += this.target.getAccelleration() * this.horizontal_vector * (deltaTime/1000);
+      if(Math.sign(this.target.horizontal_accelleration) != Math.sign(this.horizontal_vector)) {
+        this.decellerateHorizontal(deltaTime);
+      }
       this.clampHorizontal();
       this.horizontal_vector = 0;
+    } else if( this.horizontal_vector == 0 && this.target.horizontal_accelleration != 0){
+      this.decellerateHorizontal(deltaTime);
+      this.clampHorizontal();
     }
     if(this.vertical_vector != 0) {
       this.target.vertical_accelleration += this.target.getAccelleration() * this.vertical_vector * (deltaTime/1000);
+      if(Math.sign(this.target.vertical_accelleration) != Math.sign(this.vertical_vector)) {
+        this.decellerateHorizontal(deltaTime);
+      }
       this.clampVertical();
       this.vertical_vector = 0;
+    } else if( this.horizontal_vector == 0 && this.target.horizontal_accelleration != 0){
+      this.decellerateVertical(deltaTime);
+      this.clampHorizontal();
     }
     if(this.target.horizontal_accelleration != 0 || this.target.vertical_accelleration != 0){
       this.target.position.x += this.target.horizontal_accelleration * (deltaTime/1000);
