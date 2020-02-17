773cbe8bacb6fe550c6e7774e373281fd4e45c2a
Dan <codetown@gmail.com>
2020-02-16 19:29:06 -0500

    Fix bugs with decelleration.

--- src/behaviours/player/InputBehaviour.ts
+++ src/behaviours/player/InputBehaviour.ts
@@ -59,6 +59,7 @@ export class InputBehaviour extends Behaviour {
   }
 
   public onUpdate(deltaTime:number) {
+
     if(this.horizontal_vector != 0) {
       this.target.horizontal_accelleration += this.target.getAccelleration() * this.horizontal_vector * (deltaTime/1000);
       if(Math.sign(this.target.horizontal_accelleration) != Math.sign(this.horizontal_vector)) {
@@ -70,17 +71,19 @@ export class InputBehaviour extends Behaviour {
       this.decellerateHorizontal(deltaTime);
       this.clampHorizontal();
     }
+
     if(this.vertical_vector != 0) {
       this.target.vertical_accelleration += this.target.getAccelleration() * this.vertical_vector * (deltaTime/1000);
       if(Math.sign(this.target.vertical_accelleration) != Math.sign(this.vertical_vector)) {
-        this.decellerateHorizontal(deltaTime);
+        this.decellerateVertical(deltaTime);
       }
       this.clampVertical();
       this.vertical_vector = 0;
-    } else if( this.horizontal_vector == 0 && this.target.horizontal_accelleration != 0){
+    } else if( this.vertical_vector == 0 && this.target.vertical_accelleration != 0){
       this.decellerateVertical(deltaTime);
-      this.clampHorizontal();
+      this.clampVertical();
     }
+    
     if(this.target.horizontal_accelleration != 0 || this.target.vertical_accelleration != 0){
       this.target.position.x += this.target.horizontal_accelleration * (deltaTime/1000);
       this.target.position.y += this.target.vertical_accelleration * (deltaTime/1000);
