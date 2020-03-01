ceac268ff8994b04fb9d88a0f2f6407aa10d20e8
Dan <codetown@gmail.com>
2020-02-29 20:26:26 -0500

    Fix issue with schema type mismatch. Caused by a problem upstream in colyseus.

--- schema-unity/Bullet.cs
+++ schema-unity/Bullet.cs
@@ -2,16 +2,16 @@
 // THIS FILE HAS BEEN GENERATED AUTOMATICALLY
 // DO NOT CHANGE IT MANUALLY UNLESS YOU KNOW WHAT YOU'RE DOING
 // 
-// GENERATED USING @colyseus/schema 0.5.32
+// GENERATED USING @colyseus/schema 0.5.33
 // 
 
 using Colyseus.Schema;
 
 public class Bullet : Entity {
-	[Type(0, "number")]
+	[Type(7, "number")]
 	public float blastRadius = 0;
 
-	[Type(1, "string")]
+	[Type(8, "string")]
 	public string bulletMesh = "";
 }
 
--- schema-unity/Enemy.cs
+++ schema-unity/Enemy.cs
@@ -2,13 +2,13 @@
 // THIS FILE HAS BEEN GENERATED AUTOMATICALLY
 // DO NOT CHANGE IT MANUALLY UNLESS YOU KNOW WHAT YOU'RE DOING
 // 
-// GENERATED USING @colyseus/schema 0.5.32
+// GENERATED USING @colyseus/schema 0.5.33
 // 
 
 using Colyseus.Schema;
 
 public class Enemy : Entity {
-	[Type(0, "string")]
+	[Type(7, "string")]
 	public string modelType = "";
 }
 
